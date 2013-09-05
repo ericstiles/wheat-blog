b31e8cb26938735e682d59386e947814f54e3159
Eric Stiles <ets04uga@yahoo.com>
2013-09-05 09:58:50 -0500

    switched to pointing to modified wheat code

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 // Just a basic server setup for this site
 var Stack = require('stack'),
-    Creationix = require('creationix'),
-    Http = require('http'),
-    Logger = require('./logger');
+  Creationix = require('creationix'),
+  Http = require('http'),
+  Logger = require('./logger');
 
 console.log(__dirname);
 
 Http.createServer(Stack(
   Logger.log(),
-  require('wheat')(__dirname +"/..")
+  require('../wheat')(__dirname +"/..")
 )).listen(8000);
 
 Http.createServer(Stack(
-    Logger.log(),
-    require('wheat')(__dirname +"/..")
+  Logger.log(),
+  require('../wheat')(__dirname +"/..")
 )).listen(8443);
 
 
