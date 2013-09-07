bbd26739de62c94e164244169fd03f42fa820abd
Eric Stiles <ets04uga@yahoo.com>
2013-09-07 15:37:03 -0500

    Adding config file as a starting point

---
+++ config.js
@@ -0,0 +1,12 @@
+//config.route = {[]};
+
+module.exports = {
+    author : {
+        'name':'Eric Stiles',
+        'file':'eric.markdown',
+        'url': 'eric'
+    },
+    header : {
+        'Cache-Control' : 1
+    }
+};
